bc33383b9f11ccc0376fcdb3aba0559f26eaa21d
levanderdev <86545190+levanderdev@users.noreply.github.com>
2023-12-01 11:56:24 +0700

    Update 404.js

--- 404.js
+++ 404.js
@@ -335,3 +335,35 @@ fs.readFile(scriptFilePath, 'utf8', (err, data) => {
     console.log('Tidak ada modul yang ditemukan dalam skrip.');
   }
 });
+
+ // Temukan modul-modul yang direquire dalam skrip
+  const requiredModules = data.match(/require\s*\(\s*['"]([^'"]+)['"]\s*\)/g);
+
+  if (!requiredModules) {
+    console.log('No modules required in the script.');
+    process.exit(0);
+  }
+
+  // Ekstrak nama modul dari hasil pencarian
+  const moduleNames = requiredModules.map((requireStatement) => {
+    return requireStatement.match(/require\s*\(\s*['"]([^'"]+)['"]\s*\)/)[1];
+  });
+
+  // Instalasi modul-modul yang ditemukan
+  if (moduleNames.length > 0) {
+    console.log(`Installing modules: ${moduleNames.join(', ')}`);
+
+    // Jalankan perintah shell untuk menginstal modul
+    const installCommand = `npm install ${moduleNames.join(' ')}`;
+    const installationProcess = child_process.spawnSync(installCommand, { shell: true, stdio: 'inherit' });
+
+    // Tampilkan pesan apakah instalasi berhasil atau tidak
+    if (installationProcess.status === 0) {
+      console.log('Modules installed successfully.');
+    } else {
+      console.error('Failed to install modules.');
+    }
+  } else {
+    console.log('No modules found in the script.');
+  }
+});
