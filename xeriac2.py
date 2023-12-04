61e94409b1085c4c214fd32538145357e8fb33ba
levanderdev <86545190+levanderdev@users.noreply.github.com>
2023-12-04 11:56:51 +0700

    Update xeriac2.py

--- xeriac2.py
+++ xeriac2.py
@@ -145,22 +145,27 @@ def layer7():
     clear()
     si()
     print(f'''
-               ╔═══════════════════════╗
-               ║ - - M E T H O D S - - ║
- ╔═╦═══════════╬═══════════╦═══════════╬═══════════╦═╗
- ║A║  HTTP-API ║  HTTP-PRO ║   ovh     ║  HTTP-GET ║R║
- ║P║           ║   TLS     ║           ║  HTTP-POST║A║
- ║I║           ║  xeriatls ║           ║  HTTP-RAW ║W║
- ╚═╣           ║ STRONG-GO ║           ║           ╠═╝
-   ║           ║   Tlsv1   ║           ║           ║
-   ║           ║   BYPASS  ║           ║           ║
-   ║           ║           ║           ║           ║
-   ║           ║           ║           ║           ║
-   ║           ║           ║           ║           ║
-   ║           ║           ║           ║           ║
-   ║           ║           ║           ║           ║
-   ║           ║           ║           ║           ║
-''')
+\033[37m           ,MMM\033[35m8&&&.           WELCOME TO - XeriaC2
+\033[37m      _...MMMMM\033[35m88&&&&..._      KETHEK Xeria
+\033[37m   .::'''MMMMM8\033[35m8&&&&&&'''::.   EXECUTOR TEAM Version, 1.2
+\033[37m  ::     MMMMM8\033[35m8&&&&&&     ::  2023 - 2024
+\033[37m  '::....MMMMM8\033[35m8&&&&&&....::'
+\033[37m     `''''MMMMM\033[35m88&&&&''''`
+\033[37m           'MMM\033[35m8&&&'
+
+\033[37m[ LAYER - 7 ]
+\033[35mNOTE USE:
+METHODE [URL] [PORT] [TIME]
+
+\033[37m – STRIKE   – TLSV3 
+ – BOMB2    – JAVA
+ – BOMB     – HTTP
+ – GOLDEN   – MIX
+ – UAM 
+ – TLS 
+ – TLSV2
+
+""")
 
 def layer4():
     clear()
@@ -191,30 +196,28 @@ def amp_games():
 
 
 def menu():
-    sys.stdout.write(f"         \x1b]2;Xeria C2 --> Admin: [1] | Online Users: [1] | Methods: [10] | Bypass: [3] | Amps: [1]\x07")
+    sys.stdout.write(f"\x1b]2;[/] XeriaC2 :: Server Online 500 :: Online 1 :: Running: 0/10\x07")
     clear()
     print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mXeria \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to XeriaC2 \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner : Xeria \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.0')
     print("")
     print("""
  
-                \x1b[38;2;0;212;14m╔═══════════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════════╗
-                \x1b[38;2;0;212;14m║          \x1b[38;2;239;239;239mWelcome to Xeria C2 DDoS Panel      \x1b[38;2;0;49;147m║
-                \x1b[38;2;0;212;14m║ \x1b[38;2;0;49;147m- - - - - - \x1b[38;2;239;239;239mBest XeriaC2 DDoS Panel\x1b[38;2;0;212;14m - - - - -\x1b[38;2;0;49;147m║
-                \x1b[38;2;0;212;14m╚═══════════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════════╝
-                    \x1b[38;2;0;212;14m╔═══════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════╗
-                    \x1b[38;2;0;212;14m║ \x1b[38;2;239;239;239m            Best XeriaC2             \x1b[38;2;0;49;147m║
-                    \x1b[38;2;0;212;14m╚═══════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════╝
-                \x1b[38;2;0;212;14m╔═══════════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════════╗
-                \x1b[38;2;0;212;14m║   \x1b[38;2;239;239;239m   Type help to see the all commands.      \x1b[38;2;0;49;147m║
-                \x1b[38;2;0;212;14m╚═══════════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════════╝             
-    
- 
+\033[37m           ,MMM\033[35m8&&&.           WELCOME TO - XeriaC2
+\033[37m      _...MMMMM\033[35m88&&&&..._      KETHEK Xeria
+\033[37m   .::'''MMMMM8\033[35m8&&&&&&'''::.   EXECUTOR TEAM Version, 1.2
+\033[37m  ::     MMMMM8\033[35m8&&&&&&     ::  2023 - 2024
+\033[37m  '::....MMMMM8\033[35m8&&&&&&....::'
+\033[37m     `''''MMMMM\033[35m88&&&&''''`
+\033[37m           'MMM\033[35m8&&&'
+
+
+\x1b[1;37mᴘʟᴇᴀsᴇ ᴛʏᴘᴇ " HELP " ᴛᴏ sᴇᴇ ᴀʟʟ ᴛʜᴇ ᴍᴇᴛʜᴏᴅs.
 """)
 
 def main():
     menu()
     while(True):
-        cnc = input('''\x1b[38;2;0;212;14m╔══Xeria@C2\n╚==>''')
+        cnc = input("\033[0;30;45mXeriaC2@admin\x1b[1;37m\033[0m:~# \x1b[1;37m\033[0m ")
 
         if cnc == "layer7" or cnc == "LAYER7" or cnc == "L7" or cnc == "l7":
             layer7()
@@ -763,16 +766,31 @@ def main():
 
         elif "help" in cnc:
             print(f'''
-LAYER7  ► SHOW LAYER7 METHODS
-LAYER4  ► SHOW LAYER4 METHODS
-AMP     ► SHOW AMP METHODS
-SPECIAL ► SHOW SPECIAL METHODS
-BANNERS ► SHOW BANNERS
-RULES   ► RULES PANEL
-PORTS   ► SHOW ALL PORTS
-TOOLS   ► SHOW TOOLS
-CLEAR   ► CLEAR TERMINAL
-            ''')
+\033[37m           ,MMM\033[35m8&&&.           WELCOME TO - XeriaC2
+\033[37m      _...MMMMM\033[35m88&&&&..._      KETHEK Xeria
+\033[37m   .::'''MMMMM8\033[35m8&&&&&&'''::.   EXECUTOR TEAM Version, 1.2
+\033[37m  ::     MMMMM8\033[35m8&&&&&&     ::  2023 - 2024
+\033[37m  '::....MMMMM8\033[35m8&&&&&&....::'
+\033[37m     `''''MMMMM\033[35m88&&&&''''`
+\033[37m           'MMM\033[35m8&&&'
+
+\033[37m[ MENU BIGBANG-PANNEL ]
+HOW TO USE TYPE L7 OR L4 TO SEE COMMANDS
+
+[LAYER-7]       [LAYER-4]       [LAYER-12]      [VVIP]
+\033[35m• STRIKE        • STRESS        • BRUTAL        • HTTPS [\033[32mVVIP\033[35m]
+• BOMB2         • TCP           • BOT           • HTTPS2 [\033[32mVVIP\033[35m]
+• BOMB          • OVH           • BOT2 [\033[32mVIP\033[35m]    • BYPASS [\033[32mVVIP\033[35m]
+• GOLDEN                                        • BROWSER [\033[32mVVIP\033[35m]
+• UAM                                           • KILLNET [\033[32mVVIP\033[35m]
+• TLS [\033[32mVIP\033[35m]
+• TLSV2 [\033[32mVIP\033[35m]
+• TLSV3 [\033[32mVIP\033[35m]
+• JAVA
+• HTTP [\033[32mVIP\033[35m]
+• MIX [\033[32mVIP\033[35m]
+
+""")
 
         else:
             try:
